76d655b8f53f3d73b83dc7c994a26708bee31e4a
Ashutosh <ashusah043@gmail.com>
2025-06-13 12:55:17 +0530

    added new route

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ app.set('view engine', 'ejs')
 app.get('/', (req,res)=>{
      res.render('index');
 });
-
+ 
 app.get('/about', (req,res)=>{
      res.render('about');
 });
+
+app.get('/services', (req,res)=>{
+     res.render('about');
+});
+
+
    
 
 app.listen(port, ()=>{
